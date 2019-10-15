6334942606a94e39c434dea9a9940081c3750587
tupsu <tupsuosu@gmail.com>
2019-10-15 19:48:20 +0300

    add working keypress lock

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -367,10 +367,11 @@ globalkeys = awful.util.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey,           }, "space",  function () awful.util.spawn(terminal) end),
+    awful.key({ modkey,           }, "l", function () awful.util.spawn("slock") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Control", "Shift" }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
+    --awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
     awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
     awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
@@ -710,10 +711,3 @@ tag_by_name["Toggle"].master_width_factor = 0.7
   -- Run automatic screen locker
   -- awful.util.spawn_with_shell('~/.config/awesome/locker.sh')
 
-  -- Keypress screen lock
---awful.key({ modkey },
---    "l",
---    function ()
---        awful.util.spawn("slock")
---    end)
-    awful.key({ modkey,           }, "l", function () awful.util.spawn(slock) end)
